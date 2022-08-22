0f2fa5d40c9df9fff9debb8262a9949406d7774d
eajset <eajset@DESKTOP-4QA601C>
2022-08-22 10:20:55 -0600

    Avance para ingreso de pronostico, falta asociar a un usuario.

--- Controllers/PronosticosController.cs
+++ Controllers/PronosticosController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Mundial2022.Entidades;
 using Mundial2022.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,6 +18,13 @@ namespace Mundial2022.Controllers
         {
             this.context = context;
         }
+
+        [HttpGet]
+        public IActionResult Index(Usuario usuario)
+        {
+            return View(usuario);
+        }
+
         public IActionResult Crear()
         {
             PronosticoCreacionViewModel pronostico = new PronosticoCreacionViewModel();
@@ -24,6 +33,43 @@ namespace Mundial2022.Controllers
             pronostico.partidos = context.Partidos.Include(e => e.CEquipo1Navigation).Include(e => e.CEquipo2Navigation);
             return View(pronostico);
 
+        }
+
+        [HttpPost]
+        public IActionResult Crear(IFormCollection formCollection)
+        {
+            string uCodigo = "656";
+            IEnumerable<string> numerosPartidos = formCollection["NroPartido"];
+            IEnumerable<string> codigosEquipo1Partidos = formCollection["CEquipo1"];
+            IEnumerable<string> golesEquipo1Partidos = formCollection["QGolesE1"];
+            IEnumerable<string> golesEquipo2Partidos = formCollection["QGolesE2"];
+
+            List<ApuestasUsuario> apuestas = new List<ApuestasUsuario>();
+            for (int i = 0; i < numerosPartidos.Count(); i++)
+            {
+                apuestas.Add(new ApuestasUsuario
+                {
+                    NroApuetas = numerosPartidos.ElementAt(i),
+                    NroPartido = int.Parse(numerosPartidos.ElementAt(i)),
+                    UCodigo = uCodigo,
+                    AResultadoE1 = int.Parse(golesEquipo1Partidos.ElementAt(i)),
+                    AResultadoE2 = int.Parse(golesEquipo2Partidos.ElementAt(i))
+                });
+            }
+
+            foreach (var apuesta in apuestas)
+            {
+                try
+                {
+                    context.ApuestasUsuarios.Add(apuesta);
+                }
+                catch (Exception ex)
+                {
+                    string exs = ex.ToString();
+                }
+            }
+            context.SaveChanges();
+            return Ok();
         }
 
     }
